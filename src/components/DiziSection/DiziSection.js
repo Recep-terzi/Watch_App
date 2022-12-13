f40e95dcc290d320cc1bafa962f5321d28170ae2
Recep Terzi <recepterzi-67@hotmail.com>
2022-12-13 21:27:49 +0300

    watch-app

---
+++ src/components/DiziSection/DiziSection.js
@@ -0,0 +1,80 @@
+import React from "react";
+import "./DiziSection.Module.css";
+import need from "../../assets/need.jpg";
+const DiziSection = () => {
+  return (
+    <>
+      <div className="main-section">
+        <div className="dizi-section-title">Diziler</div>
+
+        <section id="dizi-section">
+          <div className="dizi-section-item">
+            <img
+              src={need}
+              className="dizi-section-item-title"
+              alt=" Need for speed film resimi"
+            ></img>
+          </div>
+          <div className="dizi-section-item">
+            <img
+              src={need}
+              className="dizi-section-item-title"
+              alt=" Need for speed film resimi"
+            ></img>
+          </div>
+          <div className="dizi-section-item">
+            <img
+              src={need}
+              className="dizi-section-item-title"
+              alt=" Need for speed film resimi"
+            ></img>
+          </div>
+          <div className="dizi-section-item">
+            <img
+              src={need}
+              className="dizi-section-item-title"
+              alt=" Need for speed film resimi"
+            ></img>
+          </div>
+          <div className="dizi-section-item">
+            <img
+              src={need}
+              className="dizi-section-item-title"
+              alt=" Need for speed film resimi"
+            ></img>
+          </div>
+          <div className="dizi-section-item">
+            <img
+              src={need}
+              className="dizi-section-item-title"
+              alt=" Need for speed film resimi"
+            ></img>
+          </div>
+          <div className="dizi-section-item">
+            <img
+              src={need}
+              className="dizi-section-item-title"
+              alt=" Need for speed film resimi"
+            ></img>
+          </div>
+          <div className="dizi-section-item">
+            <img
+              src={need}
+              className="dizi-section-item-title"
+              alt=" Need for speed film resimi"
+            ></img>
+          </div>
+          <div className="dizi-section-item">
+            <img
+              src={need}
+              className="dizi-section-item-title"
+              alt=" Need for speed film resimi"
+            ></img>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default DiziSection;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from "react";
+import DiziSection from "../DiziSection/DiziSection";
+
+import "./Main.Module.css";
+const Main = () => {
+  const videoEl = useRef(null);
+
+  const attemptPlay = () => {
+    videoEl &&
+      videoEl.current &&
+      videoEl.current.play().catch((error) => {
+        console.error("Error attempting to play", error);
+      });
+  };
+
+  useEffect(() => {
+    attemptPlay();
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <nav>
+          <div className="nav-item">
+            <a href="">Giriş</a>
+            <a href="">Çıkış</a>
+            <a href="">Profil</a>
+          </div>
+          <div className="nav-logo">PysonFlix</div>
+          <div className="nav-item">
+            <a href="#dizi-section">Diziler</a>
+            <a href="">Filmler</a>
+            <a href="">Çocuk</a>
+          </div>
+        </nav>
+        <DiziSection id="dizi-section" />
+      </div>
+      <div className="video">
+        <video
+          playsInline
+          loop
+          muted
+          alt="All the devices"
+          src={require("../../assets/main.mp4")}
+          ref={videoEl}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Main;
