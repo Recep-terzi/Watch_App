b66c251a0f7e4ed2265396164ff70a147847e004
Recep Terzi <recepterzi-67@hotmail.com>
2022-12-28 15:45:35 +0300

    update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../../redux/watchSlice";
 import "./Navbar.Module.css";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase/config";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
 const Navbar = () => {
   const user = useSelector((state) => state.watch.user);
   const dispatch = useDispatch();
-
+  const hamburgerOpen = useRef(null);
+  const hamburgerClose = useRef(null);
+  const mobileNav = useRef(null);
   const logoutUser = () => {
     signOut(auth)
       .then(() => {
@@ -20,9 +24,23 @@ const Navbar = () => {
         console.log(error.message);
       });
   };
+  useEffect(() => {
+    const openHamburger = hamburgerOpen.current;
+    openHamburger.addEventListener("click", () => {
+      const navMobile = mobileNav.current;
+      navMobile.classList.add("open");
+    });
+  }, []);
+  useEffect(() => {
+    const closeHamburger = hamburgerClose.current;
+    closeHamburger.addEventListener("click", () => {
+      const navMobile = mobileNav.current;
+      navMobile.classList.remove("open");
+    });
+  }, []);
   return (
     <>
-      <nav>
+      <nav className="desktop-nav">
         <div className="nav-item">
           {user ? (
             <>
@@ -51,6 +69,48 @@ const Navbar = () => {
           <Link to="/"></Link>
         </div>
       </nav>
+      <div className="mobile-navbar-menu">
+        <div className="nav-logo">
+          <Link to="/">PysonFlix</Link>
+          {user && (
+            <p className="welcomeUser"> Hoşgeldin, {user.displayName}</p>
+          )}
+        </div>
+        <div ref={hamburgerOpen}>
+          <GiHamburgerMenu className="hamburger-menu-open" />
+        </div>
+      </div>
+
+      <div className="hamburger-menu-wrapper" ref={mobileNav}>
+        <div className="overlay"></div>
+
+        <div className="mobile-nav-wrapper">
+          <div ref={hamburgerClose} className="close-icon">
+            <GrClose />
+          </div>
+          <nav className="mobile-nav">
+            <div className="nav-item">
+              {user ? (
+                <>
+                  <Link to="/" onClick={logoutUser}>
+                    Çıkış Yap
+                  </Link>
+                  <Link to="/mylist">Listem</Link>
+                  <Link to="/"></Link>
+                </>
+              ) : (
+                <>
+                  <Link to="/login">Giriş</Link>
+                  <Link to="/register">Kayıt ol</Link>
+                  <Link to="/alltv">Diziler</Link>
+                  <Link to="/allfilms">Filmler</Link>
+                  <Link to="/"></Link>
+                </>
+              )}
+            </div>
+          </nav>
+        </div>
+      </div>
     </>
   );
 };
